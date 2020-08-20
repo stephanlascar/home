33ec50331adb7e142cdc4f31b53c63abdb3cc312
Stéphan Lascar <stephan@lascar.im>
2020-08-20 14:28:28 +0200

    cache api response for 1 minute

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 
 from flask import Flask, jsonify
+from flask_caching import Cache
 from pymodbus.client.sync import ModbusTcpClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
@@ -8,13 +9,15 @@ from pymodbus.payload import BinaryPayloadDecoder
 MAX_SIGNED = 2147483648
 
 application = Flask(__name__)
+cache = Cache(application, config={'CACHE_TYPE': 'simple'})
 
 
 @application.route('/api/power-consumption/solar-panel')
+@cache.cached(timeout=60)
 def hello_world():
     client = ModbusTcpClient('192.168.0.4', retries=3, unit_id=3, timeout=1000)
 
-    return jsonify(dict(
+    data = dict(
         date=datetime.datetime.fromtimestamp(_get_modbus_message(client, 30193, 2, 3).decode_32bit_int()).isoformat(),
         dc_current_input=_sanitize_modbus_value(_get_modbus_message(client, 30769, 2, 3).decode_32bit_uint()),
         dc_voltage_input=_sanitize_modbus_value(_get_modbus_message(client, 30771, 2, 3).decode_32bit_uint()),
@@ -22,7 +25,10 @@ def hello_world():
         power=_sanitize_modbus_value(_get_modbus_message(client, 30775, 2, 3).decode_32bit_uint()),
         day_yield=_sanitize_modbus_value(_get_modbus_message(client, 30535, 2, 3).decode_32bit_uint()),
         total_yield=_sanitize_modbus_value(_get_modbus_message(client, 30529, 2, 3).decode_32bit_uint())
-    ))
+    )
+    client.close()
+    print('ototototto')
+    return jsonify(data)
 
 
 def _get_modbus_message(client, adress, count, unit):
